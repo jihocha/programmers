868deb2107a7d1c9b3052b23a877b3cc08f5d19d
jihocha <87089496+jihocha@users.noreply.github.com>
2026-08-24 16:50:43 +0900

    [level 0] Title: 배열 조각하기, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
@@ -1,9 +1,10 @@
 def solution(arr, query):
-    cnt = 0
-    for i in query:
-        cnt += 1
-        if cnt % 2 != 0:
-            arr = arr[:i+1]
+    p1, p2 = 0, len(arr)
+    
+    for i, ind in enumerate(query):
+        if i % 2 == 0:
+            p2 = p1 + ind + 1
         else:
-            arr = arr[i:]
-    return arr
+            p1 = p1 + ind
+            
+    return arr[p1:p2]
